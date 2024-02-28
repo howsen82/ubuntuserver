74de5abcc65784e9a4144e81a00969eb4729aada
howsen82 <howsenpl@hotmail.com>
2024-02-28 23:22:01 +0800

    Initial commit

--- 123. Setup Network Time Server.ps1
+++ 123. Setup Network Time Server.ps1
@@ -1,3 +1,5 @@
+# -----------------
+# Server
 # confirm current setting (follows are default settings)
 Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\w32time\TimeProviders\NtpServer'
 
@@ -26,3 +28,26 @@ New-NetFirewallRule `
 -Program Any `
 -LocalAddress Any `
 -LocalPort 123
+
+# -----------------
+# Client
+# confirm current synchronization NTP Server
+> w32tm /query /source
+
+# change target NTP Server (replace to your timezone server)
+# number means
+# 0x01 : SpecialInterval
+# 0x02 : UseAsFallbackOnly
+# 0x04 : SymmetricActive
+# 0x08 : NTP request in Client mode
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\w32time\Parameters" -Name "NtpServer" -Value "ntp.nict.jp,0x8"
+Restart-Service w32Time
+
+# re-sync manually
+w32tm /resync
+
+# verify status
+> w32tm /query /status
+
+(Get-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Services\w32time\Parameters").GetValue("Type")
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\w32time\Parameters" -Name "Type" -Value "NTP"
