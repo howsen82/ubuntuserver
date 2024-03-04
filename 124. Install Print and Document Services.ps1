d21c3a947bd86c019da72c0dbbc98d31264b6450
howsen82 <howsenpl@hotmail.com>
2024-03-04 23:19:01 +0800

    Initial commit

--- 124. Install Print and Document Services.ps1
+++ 124. Install Print and Document Services.ps1
@@ -1,2 +1,11 @@
 # Print and Document Services installation
 Install-WindowsFeature Print-Services -IncludeManagementTools
+
+# Install printer to print server
+Add-Printer -Name 'Microsoft_Print' -DriverName 'Microsoft Print To PDF' -Port LPT1:
+
+# Share printer
+Set-Printer -Name 'Microsoft_Print' -Shared $True -Published $True -ShareName 'MicrosoftPrint'
+
+# Add printer driver
+Add-PrinterDriver -Name 'Driver Name' -ComputerName "PrintServer" -InfPath 'C:\Drivers'
